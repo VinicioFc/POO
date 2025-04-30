405012e6f15cfa44ebde12936c2f1b47c655cbcb
VinicioFc <viniciocullqui2008@gmail.com>
2025-04-29 20:49:17 -0500

    Add files via upload

---
+++ CLASE1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase1;
+
+/**
+ *
+ * @author pc
+ */
+public class CLASE1 {
+    public static void main(String[] args) {
+        Telefono L1 = new Telefono();
+        L1.modelo = "Iphone";
+        L1.Capacidad = 128 ; 
+        L1.Color = "Negro";
+        L1.Ios = "18.4.1"; 
+
+        L1.mostrarInf();
+    }
+}
---
+++ Telefono.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clase1;
+
+/**
+ *
+ * @author pc
+ */
+public class Telefono {
+    int Capacidad;
+    String Color;
+    String Ios;  
+    String modelo;
+
+    public void mostrarInf(){
+        System.out.println("El telefono de Modelo " + modelo + " Color " + Color + " Tiene una IOS " + Ios);
+    }
+}
+
